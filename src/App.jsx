60ede581d2962b2b37e3ae6b3e325ce361dfc9a8
AI0616-somnath <somnath@aggregateintelligence.in>
2024-05-13 11:25:00 +0530

    feat: 🎸 timer added and error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import { Card, Form, Spinner } from "react-bootstrap"; // Import React Bootstrap components
-import source from "./assets/carriers_flightrates.json"
+import { useEffect, useState } from 'react';
+import { Card, Form, Spinner } from 'react-bootstrap'; // Import React Bootstrap components
+import source from './assets/carriers_flightrates.json';
 const App = () => {
   //const [token, setToken] = useState(null);
   const [airportData, setAirportData] = useState([]);
@@ -14,26 +14,30 @@ const App = () => {
   const [submitting, setSubmitting] = useState(false);
   const [timeoutRef, setTimeoutRef] = useState(null);
   const [loginData, setLoginData] = useState({
-    handle: "",
-    password: "",
+    handle: '',
+    password: '',
   });
   const [formData, setFormData] = useState({
-    flyFrom: "",
-    flyTo: "",
-    cabinClass: "",
-    _pos: "",
-    _currency: "",
+    flyFrom: '',
+    flyTo: '',
+    cabinClass: '',
+    _pos: '',
+    _currency: '',
     _carriers: [],
   });
+  const [elapsedTime, setElapsedTime] = useState(0);
 
+  const startTimer = () => {
+    setElapsedTime((prevTime) => prevTime + 5);
+  };
   const loginAndFetchData = async () => {
     try {
       const response = await fetch(
-        "https://flightrates-api.aggregateintelligence.com/api/v1/login",
+        'https://flightrates-api.aggregateintelligence.com/api/v1/login',
         {
-          method: "POST",
+          method: 'POST',
           headers: {
-            "Content-Type": "application/json",
+            'Content-Type': 'application/json',
           },
           body: JSON.stringify({
             handle: loginData.email,
@@ -44,27 +48,27 @@ const App = () => {
       const jsonData = await response.json();
       if (jsonData.token) {
         //setToken(jsonData.token);
-        sessionStorage.setItem("Authorization", jsonData.token);
+        sessionStorage.setItem('Authorization', jsonData.token);
         setLoggedIn(true);
       } else {
-        console.error("Token not found in response");
+        console.error('Token not found in response');
       }
     } catch (error) {
-      console.error("Error logging in:", error);
+      console.error('Error logging in:', error);
     }
   };
 
   const fetchData = async () => {
     try {
-      const token = sessionStorage.getItem("Authorization");
+      const token = sessionStorage.getItem('Authorization');
       if (!token) {
         console.log(
-          "JWT token not found in session. Please login to fetch data."
+          'JWT token not found in session. Please login to fetch data.'
         );
         return;
       }
       const response = await fetch(
-        "https://flightrates-api.aggregateintelligence.com/api/v1/references/airports",
+        'https://flightrates-api.aggregateintelligence.com/api/v1/references/airports',
         {
           headers: {
             Authorization: `Bearer ${token}`, // Add JWT token to the Authorization header
@@ -74,21 +78,21 @@ const App = () => {
       const jsonData = await response.json();
       setAirportData(jsonData);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
     }
   };
 
   const fetchCabinClasses = async () => {
     try {
-      const token = sessionStorage.getItem("Authorization");
+      const token = sessionStorage.getItem('Authorization');
       if (!token) {
         console.log(
-          "JWT token not found in session. Please login to fetch data."
+          'JWT token not found in session. Please login to fetch data.'
         );
         return;
       }
       const response = await fetch(
-        "https://flightrates-api.aggregateintelligence.com/api/v1/references/cabinclasses",
+        'https://flightrates-api.aggregateintelligence.com/api/v1/references/cabinclasses',
         {
           headers: {
             Authorization: `Bearer ${token}`, // Add JWT token to the Authorization header
@@ -98,21 +102,21 @@ const App = () => {
       const jsonData = await response.json();
       setCabinClass(jsonData);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
     }
   };
 
   const fetchPosData = async () => {
     try {
-      const token = sessionStorage.getItem("Authorization");
+      const token = sessionStorage.getItem('Authorization');
       if (!token) {
         console.log(
-          "JWT token not found in session. Please login to fetch data."
+          'JWT token not found in session. Please login to fetch data.'
         );
         return;
       }
       const response = await fetch(
-        " https://flightrates-api.aggregateintelligence.com/api/v1/references/poses",
+        ' https://flightrates-api.aggregateintelligence.com/api/v1/references/poses',
         {
           headers: {
             Authorization: `Bearer ${token}`, // Add JWT token to the Authorization header
@@ -122,21 +126,21 @@ const App = () => {
       const jsonData = await response.json();
       setposValue(jsonData);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
     }
   };
 
   const fetchCurrency = async () => {
     try {
-      const token = sessionStorage.getItem("Authorization");
+      const token = sessionStorage.getItem('Authorization');
       if (!token) {
         console.log(
-          "JWT token not found in session. Please login to fetch data."
+          'JWT token not found in session. Please login to fetch data.'
         );
         return;
       }
       const response = await fetch(
-        " https://flightrates-api.aggregateintelligence.com/api/v1/references/currencies",
+        ' https://flightrates-api.aggregateintelligence.com/api/v1/references/currencies',
         {
           headers: {
             Authorization: `Bearer ${token}`, // Add JWT token to the Authorization header
@@ -146,9 +150,9 @@ const App = () => {
       const jsonData = await response.json();
       setCurrency(jsonData);
     } catch (error) {
-      console.error("Error fetching data:", error);
+      console.error('Error fetching data:', error);
     }
-  }
+  };
   // fetchPosData();
   // useEffect(() => {
   //   loginAndFetchData();
@@ -176,74 +180,85 @@ const App = () => {
       ],
       _cabinClasses: [formData.cabinClass],
       _pos: formData._pos,
-      _source: "64ae65cfd06e77f95bfefd95",
+      _source: '64ae65cfd06e77f95bfefd95',
       _carriers: [formData._carriers],
       _currency: formData._currency,
-      deliveryMode: ["db"],
+      deliveryMode: ['db'],
     };
 
     const staticData = {
       isRoundTrip: false,
       los: 1,
-      horizons: ["02/03/2024"],
+      horizons: ['02/03/2024'],
       pax: {
         adults: 1,
         infants: 0,
         children: 0,
       },
-      noOfStops: "1",
+      noOfStops: '1',
       duration: {
         hour: 40,
         minute: 30,
       },
-      fareType: "Doctors",
+      fareType: 'Doctors',
     };
     const finalData = { ...dynamicData, ...staticData };
-    const token = sessionStorage.getItem("Authorization");
+    const token = sessionStorage.getItem('Authorization');
     if (!token) {
       console.log(
-        "JWT token not found in session. Please login to fetch data."
+        'JWT token not found in session. Please login to fetch data.'
       );
       return;
     }
     // Post finalData to the API endpoint
-    await fetch("https://flightrates-api.aggregateintelligence.com/api/v1/realtime", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`
-      },
-      body: JSON.stringify(finalData),
-    }).then(response => response.json())
-      .then(data => setShopId(data))
-      .catch(error => console.error('Error fetching data:', error));
-  }
+    await fetch(
+      'https://flightrates-api.aggregateintelligence.com/api/v1/realtime',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(finalData),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => setShopId(data))
+      .catch((error) => console.error('Error fetching data:', error));
+  };
 
   useEffect(() => {
     // Define the checkStatus function inside the useEffect
     const checkStatus = async () => {
-      const token = sessionStorage.getItem("Authorization");
+      const token = sessionStorage.getItem('Authorization');
       if (shopId !== null) {
         console.log(shopId);
         try {
           // Fetch the status of the shopId
-          const response = await fetch(`https://flightrates-api.aggregateintelligence.com/api/v1/shop/status/${shopId.shopId}`, {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          });
+          const response = await fetch(
+            `https://flightrates-api.aggregateintelligence.com/api/v1/shop/status/${shopId.shopId}`,
+            {
+              headers: {
+                Authorization: `Bearer ${token}`,
+              },
+            }
+          );
           const data = await response.json();
           // Check if the status is not completed
           if (data.status !== 'completed') {
-            setStatus(`Shop Id: ${shopId.shopId} - Checking for every Five Sec`);
-            console.log("STATUS IS STILL NOT COMPLETED", data.status);
+            setStatus(
+              `Shop Id: ${shopId.shopId} - Checking for every Five Sec`
+            );
+            console.log('STATUS IS STILL NOT COMPLETED', data.status);
             // Use a timeout to call the function again in 5 seconds
+            startTimer();
             setTimeoutRef(setTimeout(checkStatus, 5000));
           } else {
             // Handle the completed status
             setStatus('completed');
-            console.log("STATUS IS COMPLETED", data.status);
-            window.alert("Shop status is completed!"); // Handle the completion
+            console.log('STATUS IS COMPLETED', data.status);
+            window.alert('Shop status is completed!');
+            clearTimeout(timeoutRef); // Handle the completion
           }
         } catch (error) {
           console.error('Error checking shop status:', error);
@@ -256,8 +271,8 @@ const App = () => {
     }
     // Cleanup function to clear timeout when shopId changes or the component unmounts
     return () => {
-      if (setTimeoutRef) {
-        clearTimeout(setTimeoutRef);
+      if (timeoutRef) {
+        clearTimeout(timeoutRef);
       }
     };
   }, [shopId]);
@@ -265,7 +280,8 @@ const App = () => {
     setShowStatus(false);
     setSubmitting(false); // Reset submission process state
     clearTimeout(timeoutRef);
-    setStatus('');  // Clear the setTimeout() function
+    setStatus('');
+    setElapsedTime(0); // Clear the setTimeout() function
   };
   const handleChange = (event) => {
     const { name, value } = event.target;
@@ -317,8 +333,7 @@ const App = () => {
                 <button
                   type="button"
                   onClick={loginAndFetchData}
-                  className="btn btn-primary"
-                >
+                  className="btn btn-primary">
                   Login
                 </button>
               )}
@@ -331,8 +346,10 @@ const App = () => {
                       name="flyFrom"
                       onChange={handleChange}
                       value={formData.flyFrom}
-                      className="mb-3 custom-select-sm"
-                    >
+                      className="mb-3 custom-select-sm">
+                      <option value={null} selected>
+                        Select
+                      </option>
                       {airportData?.airports?.map((airport) => (
                         <option value={airport._id} key={airport._id}>
                           {airport.airportName}
@@ -347,8 +364,10 @@ const App = () => {
                       name="flyTo"
                       onChange={handleChange}
                       value={formData.flyTo}
-                      className="mb-3 custom-select-sm"
-                    >
+                      className="mb-3 custom-select-sm">
+                      <option value={null} selected>
+                        Select
+                      </option>
                       {airportData?.airports?.map((airport) => (
                         <option value={airport._id} key={airport._id}>
                           {airport.airportName}
@@ -363,8 +382,10 @@ const App = () => {
                       name="cabinClass"
                       onChange={handleChange}
                       value={formData.cabinClass}
-                      className="mb-3 custom-select-sm"
-                    >
+                      className="mb-3 custom-select-sm">
+                      <option value={null} selected>
+                        Select
+                      </option>
                       {cabinClass?.cabinclasses?.map((cabin) => (
                         <option value={cabin._id} key={cabin._id}>
                           {cabin.name}
@@ -379,8 +400,10 @@ const App = () => {
                       name="_pos"
                       onChange={handleChange}
                       value={formData._pos}
-                      className="mb-3 custom-select-sm"
-                    >
+                      className="mb-3 custom-select-sm">
+                      <option value={null} selected>
+                        Select
+                      </option>
                       {posValue?.poses?.map((pos) => (
                         <option value={pos._id} key={pos._id}>
                           {pos.countryCode}
@@ -395,8 +418,10 @@ const App = () => {
                       name="_currency"
                       onChange={handleChange}
                       value={formData._currency}
-                      className="mb-3 custom-select-sm"
-                    >
+                      className="mb-3 custom-select-sm">
+                      <option value={null} selected>
+                        Select
+                      </option>
                       {currency?.currencies?.map((cur) => (
                         <option value={cur._id} key={cur._id}>
                           $ {cur.iso}
@@ -411,8 +436,10 @@ const App = () => {
                       name="_carriers"
                       onChange={handleChange}
                       value={formData._carriers}
-                      className="mb-3 custom-select-sm"
-                    >
+                      className="mb-3 custom-select-sm">
+                      <option value={null} selected>
+                        Select
+                      </option>
                       {source.map((s) => (
                         <option value={s._id.$oid} key={s._id.$oid}>
                           {s.source}
@@ -422,7 +449,10 @@ const App = () => {
                   </Form.Group>
 
                   {submitting && <Spinner animation="border" role="status" />}
-                  <div className={`Status: ${showStatus ? 'in-progress' : 'hidden'}`}>
+                  {submitting && <div>Elapsed Time: {elapsedTime} seconds</div>}
+                  <div
+                    className={`Status: ${showStatus ? 'in-progress' : 'hidden'
+                      }`}>
                     {status}
                   </div>
                   {loggedIn && (
